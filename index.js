49d8790988f9fef8166100f49e726dccf1212c66
StevenJohnsonPersonal <56038965+StevenJohnsonPersonal@users.noreply.github.com>
2021-12-26 08:25:28 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const PORT = 8000;
 let gpioPins = [2, 3, 4, 17, 27, 22, 10, 9, 11, 5, 6, 13, 19, 26, 14, 15, 18, 23, 24, 25, 8, 7, 12, 16, 20, 21];
 // Initialize the pins to be used later
 let outputPins = [];
-// for(var i = 0; i < gpioPins.length; i++)
-// {
-//     outputPins.push(new gpio(gpioPins[i], 'out'));
-// }
+
+for(var i = 0; i < gpioPins.length; i++)
+{
+    console.log(gpioPins[i]);
+    outputPins.push(new gpio(gpioPins[i], 'out'));
+}
 
 for(var i = 0; i < 1000; i++)
 {
@@ -30,9 +32,82 @@ app.get("/", function(request, response) {
 
 app.post("/TogglePin", function(request, response) {
     // Get the pin to toggle
-    var pin = request.body["pinNumber"];
+    var pin = request.body["pinIndex"];
     // XOR the current value with 1 to toggle
     outputPins[pin].writeSync(outputPins[pin].readSync() ^ 1);
+    console.log("Pin " + gpioPins[pin] + " is turned " + (outputPins[pin].readSync() == 1 ? "on" : "off"));
+    response.sendStatus(200);
+});
+
+var timeoutTime = 5;
+var timeoutList = [];
+app.post("/TimeoutPin", function(request, response) {
+    var pin = request.body["pinIndex"];
+    if(outputPins[pin].readSync() == 1)
+    {
+        for(var i = 0; i < timeoutList.length; i++) {
+            if(timeoutList[i][1] == pin) {
+                response.end(JSON.stringify({ pinRunning: true, timeLeft: getTimeLeft(timeoutList[i][0]) }));
+                return;
+            }
+        }
+        return;
+    }
+    console.log("Pin " + gpioPins[pin] + " has been turned on");
+    outputPins[pin].writeSync(1);
+    timeoutList.push([setTimeout(() => {
+        console.log("Pin " + gpioPins[pin] + " has been turned off");
+        outputPins[pin].writeSync(0);
+        for(var i = 0; i < timeoutList.length; i++) {
+            if(timeoutList[i][1] == pin) {
+                timeoutList.splice(i, 1);
+                break;
+            }
+        }
+    }, timeoutTime * 1000), pin]);
+    console.log(timeoutList);
+    response.end(JSON.stringify({ pinRunning: true, timeLeft: timeoutTime }));
+});
+
+app.post('/SetTimeout', function(request, response) {
+    try {
+        timeoutTime = request.body['timeoutValue'];
+        response.sendStatus(200);
+    }
+    catch(error) {
+        response.sendStatus(400);
+        console.log(error.message);
+    }
+    
+});  
+
+app.post("/TurnPinOn", function(request, response) {
+    var pin = request.body["pinIndex"];
+    // check to see if it is in the list of active pins
+    for(var i = 0; i < timeoutList.length; i++) {
+        if(timeoutList[i] == pin) {
+            response.sendStatus(500);
+            return;
+        }
+    }
+    outputPins[pin].writeSync(1);
+    console.log(gpioPins[pin] + " has been turned on");
+    timeoutList.push(pin);
+    response.sendStatus(200);
+});
+
+app.post("/TurnPinOff", function(request, response) {
+    var pin = request.body["pinIndex"];
+    outputPins[pin].writeSync(0);
+    console.log(gpioPins[pin] + " has been turned off");
+    // Remove pin from active pins
+    for(var i = 0; i < timeoutList.length; i++) {
+        if(timeoutList[i] == pin) {
+            timeoutList.splice(i, 1);
+            break;
+        }
+    }
+    console.log(timeoutList);
     response.sendStatus(200);
 });
 
--- public/lcars-classic.js
+++ public/lcars-classic.js
@@ -9,9 +9,88 @@ let computerSounds = [
     './sounds/computerbeep_41.mp3',
 ];
 
+let timeoutValues = [5, 10, 15, 20, 30, 40, 60];
+let timeoutIndex = 0;
+
 function PlaySound() {
     let soundClip = computerSounds[Math.floor(Math.random() * 9)];
     var sound = new Audio(soundClip);
     sound.currentTime = 0;
     sound.play();
 }
+
+async function TogglePin(pinIndex) {
+    PlaySound();
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ pinIndex: pinIndex })
+    };
+    await fetch('/TogglePin', requestOptions)
+        .then((response) => {
+            if(response.ok) {
+                return;
+            } else {
+                alert("Error");
+            }
+        });
+}
+
+async function TimeoutPin(element, pinIndex) {
+    PlaySound();
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ pinIndex: pinIndex })
+    };
+    var breakValue = false;
+    await fetch('/TurnPinOn', requestOptions)
+        .then((response) => {
+            if(!response.ok) {
+                breakValue = true;
+                return;
+            }
+        });
+    if(breakValue == true)
+    {
+        return;
+    }
+    var timeToWait = timeoutValues[timeoutIndex];
+    for(var i = 0; i < timeToWait; i++)
+    {
+        element.children[0].innerHTML = '-' + (timeToWait - i) + 'M';
+        await new Promise(r => setTimeout(r, 1000));
+    }
+    element.children[0].innerHTML = '';
+    await fetch('/TurnPinOff', requestOptions)
+        .then((response) => {
+            if(!response.ok) {
+                alert("error");
+                return;
+            }
+        });
+}
+
+async function CycleTimeout() {
+    timeoutIndex = (timeoutIndex + 1) % 7;
+    document.getElementById("TimeoutValue").innerHTML = timeoutValues[timeoutIndex];
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ timeoutValue: timeoutValues[timeoutIndex] })
+    };
+    await fetch('/SetTimeout', requestOptions)
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error("Error");
+        }
+    })
+    .catch((error) => {
+        console.log("Error");
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("TimeoutValue").innerHTML = timeoutValues[timeoutIndex];
+});
